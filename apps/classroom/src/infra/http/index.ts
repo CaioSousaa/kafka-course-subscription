eef002470ec481a4f77d9773670cea1e9765a409
caio <caiothalles607@gmail.com>
2024-12-07 13:49:24 -0300

    feat: created consuming service

--- apps/classroom/src/infra/http/index.ts
+++ apps/classroom/src/infra/http/index.ts
@@ -1,11 +1,71 @@
 import "dotenv/config";
 
-import Express from "express";
+import { EnrollStudentToCourse } from "../../application/usecases/enroll-course-to-student";
+import { PrismaCoursesRepository } from "../database/prisma/repositories/prisma-courses-repository";
+import { PrismaEnrollmentRepository } from "../database/prisma/repositories/prisma-enrollments-repository";
+import { PrismaStudentsRepository } from "../database/prisma/repositories/prisma-students-repository";
+import { kafka } from "../messaging/kafka/kafka";
 
-const app = Express();
+interface PurchasesNewPurchaseMessage {
+  product: {
+    id: string;
+    title: string;
+  };
+  customer: {
+    name: string;
+    email: string;
+  };
+  purchaseId: string;
+}
 
-app.use(Express.json());
+async function main() {
+  const consumer = kafka.consumer({
+    groupId: "classroom-group",
+    allowAutoTopicCreation: true,
+  });
 
-app.listen(process.env.API_PORT || 3000, () =>
-  console.log("[CLASSROOM] server is run")
-);
+  await consumer.connect();
+  await consumer.subscribe({ topic: "purchase.new-purchase" });
+
+  await consumer.run({
+    eachMessage: async ({ message }) => {
+      const purchaseJSON = message.value?.toString();
+
+      if (!purchaseJSON) {
+        return;
+      }
+
+      const purchase: PurchasesNewPurchaseMessage = JSON.parse(purchaseJSON);
+
+      const prismaStudentsRepository = new PrismaStudentsRepository();
+      const prismaCourseRepository = new PrismaCoursesRepository();
+      const prismaEnrollmentRepository = new PrismaEnrollmentRepository();
+
+      const enrollStudentToCourse = new EnrollStudentToCourse(
+        prismaStudentsRepository,
+        prismaCourseRepository,
+        prismaEnrollmentRepository
+      );
+
+      await enrollStudentToCourse.execute({
+        student: {
+          name: purchase.customer.name,
+          email: purchase.customer.email,
+        },
+        course: {
+          title: purchase.product.title,
+          purchasesProductId: purchase.product.id,
+        },
+        purchasesEnrolledByPurchaseId: purchase.purchaseId,
+      });
+
+      console.log(
+        `[Classroom] Enrolled user ${purchase.customer.name} to ${purchase.product.title}`
+      );
+    },
+  });
+}
+
+main().then(() => {
+  console.log("[Classroom] Listening to Kafka messages");
+});
